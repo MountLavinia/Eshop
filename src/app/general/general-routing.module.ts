4662d22e21031d154998270763ccf96cb2bca461
Keerthi Srilal <ksrilalranasinghe95@gmail.com>
2021-03-13 16:28:21 +0530

    ksrilal_20210313

--- src/app/general/general-routing.module.ts
+++ src/app/general/general-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { CartComponent } from './cart/cart.component';
+import { ContactComponent } from './contact/contact.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { GeneralLandPageComponent } from './general-land-page/general-land-page.component';
 import { ShopItemComponent } from './shop-item/shop-item.component';
@@ -31,6 +32,10 @@ const routes: Routes = [
       {
         path: "About",
         component: AboutComponent
+      },
+      {
+        path: "Contact",
+        component: ContactComponent
       },
       {
         path: "",
